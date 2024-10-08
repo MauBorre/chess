8a35279acdd62a1d1a4fdb3691605f946c1848ff
MauBorre <122322896+MauBorre@users.noreply.github.com>
2024-10-08 18:04:31 -0300

    Update board.py

--- board.py
+++ board.py
@@ -1,3 +1,20 @@
+'''
+Las piezas pueden moverse en cinco direcciones:
+    > ADELANTE
+    > ATRAS
+    > IZQUIERDA
+    > DERECHA
+    > DIAGONALES
+Definidos como:
+    > NORTE = -8
+    > NORESTE = -8+1
+    > NOROESTE = -8-1
+    > SUR = +8
+    > SUROESTE = +8-1
+    > SURESTE = +8+1
+    > ESTE = +1
+    > OESTE = -1
+'''
 NORTE = -8
 NOR_ESTE = -8+1
 NOR_OESTE = -8-1
