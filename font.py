e305ec477b038310ec65039dad598eeed7991b74
MauBorre <122322896+MauBorre@users.noreply.github.com>
2024-10-13 18:01:32 -0300

    Add files via upload

---
+++ font.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.font.init()
+
+#fonts path...
+
+# Medium font
+m_font_size = 22
+medium_font = pygame.font.Font(None,m_font_size)
+
+# Large font
+l_font_size = 28
+large_font = pygame.font.Font(None,l_font_size)
