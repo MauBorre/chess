6e87fb760a0025e53053ada0276b602f8c24714a
MauBorre <122322896+MauBorre@users.noreply.github.com>
2024-11-01 20:44:13 -0300

    Update screen_drawer__.py

--- screen_drawer__.py
+++ screen_drawer__.py
@@ -1,11 +1,17 @@
 class Font_1: ...
 
 class ScreenDrawer:
+    def __init__(self, screen):
+        self.screen = screen
+
     def draw_text(): ...
 
 class MainMenuDrawer(ScreenDrawer): ...
 
 class MatchDrawer(ScreenDrawer):
+    def __init__(self, screen):
+        super().__init__(screen)
+
     def draw_match_mode(self):
         self.draw_text(self._instructions['mode'], Font_1, size=20)
 
