14ff2a08a396b8aacc3c8a95f02c9e7c9137c316
MauBorre <122322896+MauBorre@users.noreply.github.com>
2024-11-01 22:20:48 -0300

    Update screen_drawer__.py

--- screen_drawer__.py
+++ screen_drawer__.py
@@ -1,10 +1,21 @@
+import font
+
 class Font_1: ...
 
 class ScreenDrawer:
     def __init__(self, screen):
         self.screen = screen
 
-    def draw_text(): ...
+    def draw_text(self,text,color,x,y,center=True,font_size='large'):
+        _font = font.large_font if font_size=='large' else font.medium_font
+        surface = self.master.screen
+        textobj = _font.render(text,1,color)
+        text_width = textobj.get_width()
+        text_height = textobj.get_height()
+        textrect = textobj.get_rect()
+        if center: textrect.topleft = (x - text_width/2, y - text_height/2) # anchors placement at center
+        else: textrect.topleft = (x,y)
+        surface.blit(textobj,textrect)
 
 class MainMenuDrawer(ScreenDrawer): ...
 
